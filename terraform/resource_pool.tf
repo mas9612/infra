e860dc328127ad5dbed46cae47c3e4b02ea6ffe8
mas9612 <mas9612@gmail.com>
2018-10-17 13:06:41 +0900

    add etcd-cluster.tf

--- terraform/resource_pool.tf
+++ terraform/resource_pool.tf
@@ -1,3 +1,8 @@
+data "vsphere_resource_pool" "pool_35" {
+  name          = "10.1.3.35/Resources"
+  datacenter_id = "${data.vsphere_datacenter.dc_master.id}"
+}
+
 data "vsphere_resource_pool" "rp_test" {
   name          = "k800123-test"
   datacenter_id = "${data.vsphere_datacenter.dc_master.id}"
