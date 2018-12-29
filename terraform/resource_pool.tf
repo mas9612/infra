8d278670949fd9f7960ca834f1ea4c596826b42f
mas9612 <mas9612@gmail.com>
2018-12-29 22:50:41 +0900

    add nfs-server definition

--- terraform/resource_pool.tf
+++ terraform/resource_pool.tf
@@ -7,3 +7,8 @@ data "vsphere_resource_pool" "rp_test" {
   name          = "k800123-test"
   datacenter_id = "${data.vsphere_datacenter.dc_master.id}"
 }
+
+data "vsphere_resource_pool" "rp_production" {
+  name          = "k800123-production"
+  datacenter_id = "${data.vsphere_datacenter.dc_master.id}"
+}
