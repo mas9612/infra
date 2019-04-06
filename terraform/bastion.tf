180d42d2a8550512ad8929efeb184c658ad218c2
Masato Yamazaki <mas9612@gmail.com>
2019-04-06 18:14:16 +0900

    add bastion

---
+++ terraform/bastion.tf
@@ -0,0 +1,41 @@
+resource "vsphere_virtual_machine" "bastion" {
+  name             = "k800123-bastion"
+  resource_pool_id = "${data.vsphere_resource_pool.rp_k800123_35.id}"
+  datastore_id     = "${data.vsphere_datastore.ds_master.id}"
+
+  num_cpus  = 2
+  memory    = 4096
+  guest_id  = "${data.vsphere_virtual_machine.centos7.guest_id}"
+  scsi_type = "${data.vsphere_virtual_machine.centos7.scsi_type}"
+
+  network_interface {
+    network_id   = "${data.vsphere_network.nw_k800123_edge.id}"
+    adapter_type = "${data.vsphere_virtual_machine.centos7.network_interface_types[0]}"
+  }
+
+  disk {
+    label            = "disk0"
+    size             = "${data.vsphere_virtual_machine.centos7.disks.0.size}"
+    eagerly_scrub    = "${data.vsphere_virtual_machine.centos7.disks.0.eagerly_scrub}"
+    thin_provisioned = "${data.vsphere_virtual_machine.centos7.disks.0.thin_provisioned}"
+  }
+
+  clone {
+    template_uuid = "${data.vsphere_virtual_machine.centos7.id}"
+
+    customize {
+      linux_options {
+        host_name = "bastion"
+        domain    = "k800123.firefly.kutc.kansai-u.ac.jp"
+      }
+
+      network_interface {
+        ipv4_address = "172.16.1.1"
+        ipv4_netmask = 18
+      }
+
+      ipv4_gateway    = "172.16.0.31"
+      dns_server_list = ["10.1.3.21", "10.1.3.80"]
+    }
+  }
+}
