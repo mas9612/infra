6e803412bf4f25d397741343971a5b22fd75959f
Masato Yamazaki <mas9612@gmail.com>
2019-04-24 12:48:59 +0900

    add k8s-cluster.tf

--- terraform/k8s-cluster.tf
+++ terraform/k8s-cluster.tf
@@ -1,16 +1,16 @@
 variable "instance_ips" {
   default = {
-    "0" = "10.1.240.222"
-    "1" = "10.1.240.223"
-    "2" = "10.1.240.224"
+    "0" = "192.168.100.100"
+    "1" = "192.168.100.101"
+    "2" = "192.168.100.102"
   }
 }
 
 variable "instance_hostnames" {
   default = {
-    "0" = "k8s-node-01"
-    "1" = "k8s-node-02"
-    "2" = "k8s-node-03"
+    "0" = "k8s-master-01"
+    "1" = "k8s-node-01"
+    "2" = "k8s-node-02"
   }
 }
 
@@ -18,16 +18,16 @@ resource "vsphere_virtual_machine" "k8s-node" {
   count = 3
 
   name             = "k800123-${lookup(var.instance_hostnames, count.index)}"
-  resource_pool_id = "${data.vsphere_resource_pool.rp_production.id}"
+  resource_pool_id = "${data.vsphere_resource_pool.rp_test.id}"
   datastore_id     = "${data.vsphere_datastore.ds_master.id}"
 
-  num_cpus  = 2
-  memory    = 4096
+  num_cpus  = 4
+  memory    = 8192
   guest_id  = "${data.vsphere_virtual_machine.centos7.guest_id}"
   scsi_type = "${data.vsphere_virtual_machine.centos7.scsi_type}"
 
   network_interface {
-    network_id   = "${data.vsphere_network.nw_vm_network.id}"
+    network_id   = "${data.vsphere_network.nw_k800123_dc35.id}"
     adapter_type = "${data.vsphere_virtual_machine.centos7.network_interface_types[0]}"
   }
 
@@ -49,10 +49,10 @@ resource "vsphere_virtual_machine" "k8s-node" {
 
       network_interface {
         ipv4_address = "${lookup(var.instance_ips, count.index)}"
-        ipv4_netmask = 16
+        ipv4_netmask = 24
       }
 
-      ipv4_gateway    = "10.1.3.1"
+      ipv4_gateway    = "192.168.100.1"
       dns_server_list = ["10.1.3.21", "10.1.3.80"]
     }
   }
