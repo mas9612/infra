4c03cc306e3b7bb90c59ce2b8dcadfb96311aac8
Masato Yamazaki <mas9612@gmail.com>
2019-06-28 16:51:19 +0900

    terraform: add tor 33, 34 router

--- terraform/resource_pool.tf
+++ terraform/resource_pool.tf
@@ -1,3 +1,13 @@
+data "vsphere_resource_pool" "pool_33" {
+  name          = "10.1.3.33/Resources"
+  datacenter_id = "${data.vsphere_datacenter.dc_bachelor.id}"
+}
+
+data "vsphere_resource_pool" "pool_34" {
+  name          = "10.1.3.34/Resources"
+  datacenter_id = "${data.vsphere_datacenter.dc_bachelor.id}"
+}
+
 data "vsphere_resource_pool" "pool_35" {
   name          = "10.1.3.35/Resources"
   datacenter_id = "${data.vsphere_datacenter.dc_master.id}"
@@ -13,6 +23,16 @@ data "vsphere_resource_pool" "rp_production" {
   datacenter_id = "${data.vsphere_datacenter.dc_master.id}"
 }
 
+data "vsphere_resource_pool" "rp_k800123_33" {
+  name          = "14-0499 Yamazaki-33"
+  datacenter_id = "${data.vsphere_datacenter.dc_bachelor.id}"
+}
+
+data "vsphere_resource_pool" "rp_k800123_34" {
+  name          = "14-0499 Yamazaki"
+  datacenter_id = "${data.vsphere_datacenter.dc_bachelor.id}"
+}
+
 data "vsphere_resource_pool" "rp_k800123_35" {
   name          = "18M7119 Yamazaki"
   datacenter_id = "${data.vsphere_datacenter.dc_master.id}"
@@ -21,6 +41,16 @@ data "vsphere_resource_pool" "rp_k800123_35" {
 resource "vsphere_resource_pool" "rp_edge" {
   name                    = "k800123-edge"
   parent_resource_pool_id = "${data.vsphere_resource_pool.rp_k800123_35.id}"
+}
+
+resource "vsphere_resource_pool" "rp_tor33" {
+  name                    = "k800123-tor33"
+  parent_resource_pool_id = "${data.vsphere_resource_pool.rp_k800123_33.id}"
+}
+
+resource "vsphere_resource_pool" "rp_tor34" {
+  name                    = "k800123-tor34"
+  parent_resource_pool_id = "${data.vsphere_resource_pool.rp_k800123_34.id}"
 }
 
 resource "vsphere_resource_pool" "rp_tor35" {
